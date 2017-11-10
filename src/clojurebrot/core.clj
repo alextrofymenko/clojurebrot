eabe9d660c6ab9cd73ee4bb17a77a556db51257b
Alex Trofymenko <byte.shaggy@gmail.com>
2017-11-10 15:55:23 +0000

    Extracting calculation into equations further improves performance

--- src/clojurebrot/core.clj
+++ src/clojurebrot/core.clj
@@ -17,23 +17,30 @@
    :scale 0.0035
    :limit 4})
 
+(defn zx-fn [cx x scale] (+ cx (* (- x HALF_WIDTH) scale)))
+(defn zy-fn [cy y scale] (+ cy (* (- y HALF_HEIGHT) scale)))
+(defn a'-fn [a b zx] (- (* a a) (* b b) (- 0 zx)))
+(defn b'-fn [a b zy] (+ (* 2 a b) zy))
+(defn z2-fn [a' b'] (+ (* a' a') (* b' b')))
+(defn calculate-hue [n z2] (/ (+ n (- 1 (/ (Math/log (Math/log (Math/sqrt z2))) LOG2))) ITERATIONS))
+
 (defn draw-state
   [{:keys [cx cy scale limit]}]
   (q/background 0)
   (doseq [x (range WIDTH)
           y (range HEIGHT)
-          :let [zx (+ cx (* (- x HALF_WIDTH) scale))
-                zy (+ cy (* (- y HALF_HEIGHT) scale))]]
+          :let [zx (zx-fn cx x scale)
+                zy (zy-fn cy y scale)]]
     (loop [a zx
            b zy
            n 1]
-      (let [a' (- (* a a) (* b b) (- 0 zx))
-            b' (+ (* 2 a b) zy)
-            z2 (+ (* a' a') (* b' b'))]
+      (let [a' (a'-fn a b zx)
+            b' (b'-fn a b zy)
+            z2 (z2-fn a' b')]
         (cond
           (> n (dec ITERATIONS)) nil
-          (> z2 limit) (let [smooth (+ n (- 1 (/ (Math/log (Math/log (Math/sqrt z2))) LOG2)))]
-                         (q/stroke (/ smooth ITERATIONS) 0.6 1)
+          (> z2 limit) (do
+                         (q/stroke (calculate-hue n z2) 0.6 1)
                          (q/point x y))
           :else (recur a' b' (inc n)))))))
 
@@ -45,7 +52,7 @@
     :title "Mandelbrot fractal"
     :size [WIDTH HEIGHT]
     :setup setup
-    :draw #(println (time (draw-state %)))
+    :draw #(time (draw-state %))
     :middleware [m/fun-mode]
     :feature [:exit-on-close]
     :renderer :p2d))
