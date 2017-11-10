9e8719143850379b2c5d08955e2793e4762ef3fd
Alex Trofymenko <byte.shaggy@gmail.com>
2017-11-10 18:02:45 +0000

    wip

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
 (defproject clojurebrot "0.1.0-SNAPSHOT"
   :main clojurebrot.core
-  :jvm-opts ^:replace ["-server"]
+  :jvm-opts ^:replace ["-server"
+                       "-XX:+AggressiveOpts"
+                       "-XX:+UseFastAccessorMethods"
+                       "-XX:+UseCompressedOops"
+                       "-d64"]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [quil "2.6.0"]])
+                 [quil "2.6.0"]]
+  :profiles {:dev {:plugins [[lein-nodisassemble "0.1.3"]]}})
--- src/clojurebrot/core.clj
+++ src/clojurebrot/core.clj
@@ -1,6 +1,7 @@
 (ns clojurebrot.core
   (:require [quil.core :as q]
-            [quil.middleware :as m]))
+            [quil.middleware :as m])
+  (:gen-class))
 
 (def WIDTH 1280)
 (def HEIGHT 800)
@@ -8,14 +9,19 @@
 (def HALF_HEIGHT (/ HEIGHT 2))
 (def ITERATIONS 50)
 (def LOG2 (Math/log 2))
+(def coords
+  (for [x (range WIDTH)
+        y (range HEIGHT)]
+    [x y]))
 
 (defn setup []
   (q/frame-rate 5)
   (q/color-mode :hsb 1 1 1)
-  {:cx    -0.5
-   :cy    0
-   :scale 0.0035
-   :limit 4})
+  {:cx         -0.5
+   :cy         0
+   :scale      0.0035
+   :limit      4
+   :saturation 0.6})
 
 (defn zx-fn [cx x scale] (+ cx (* (- x HALF_WIDTH) scale)))
 (defn zy-fn [cy y scale] (+ cy (* (- y HALF_HEIGHT) scale)))
@@ -24,34 +30,46 @@
 (defn z2-fn [a' b'] (+ (* a' a') (* b' b')))
 (defn calculate-hue [n z2] (/ (+ n (- 1 (/ (Math/log (Math/log (Math/sqrt z2))) LOG2))) ITERATIONS))
 
+(defn set-pixel
+  [x y n z2 s b]
+  (->> (q/color (calculate-hue n z2) s b)
+       (q/set-pixel x y)))
+
+(defn mandelbrot-iter
+  [x y zx zy {:keys [limit saturation]}]
+  (loop [a zx
+         b zy
+         n 1]
+    (let [a' (a'-fn a b zx)
+          b' (b'-fn a b zy)
+          z2 (z2-fn a' b')]
+      (cond
+        (> n (dec ITERATIONS)) nil
+        (> z2 limit) (set-pixel x y n z2 saturation 1)
+        :else (recur a' b' (inc n))))))
+
+(defn get-intermediates
+  [x y {:keys [cx cy limit saturation scale] :as state}]
+  (let [zx (zx-fn cx x scale)
+        zy (zy-fn cy y scale)]
+    (mandelbrot-iter x y zx zy state)))
+
 (defn draw-state
-  [{:keys [cx cy scale limit]}]
+  [{:keys [cx cy scale limit saturation] :as state}]
   (q/background 0)
-  (doseq [x (range WIDTH)
-          y (range HEIGHT)
-          :let [zx (zx-fn cx x scale)
-                zy (zy-fn cy y scale)]]
-    (loop [a zx
-           b zy
-           n 1]
-      (let [a' (a'-fn a b zx)
-            b' (b'-fn a b zy)
-            z2 (z2-fn a' b')]
-        (cond
-          (> n (dec ITERATIONS)) nil
-          (> z2 limit) (->> (q/color (calculate-hue n z2) 0.6 1)
-                            (q/set-pixel x y))
-          :else (recur a' b' (inc n)))))))
+  (doall (map (fn [[x y]] (get-intermediates x y state)) coords)))
+
+(defn update-state [state]
+  (assoc state :saturation (+ 0.6 (- 0.1 (rand 0.2)))))
 
 (defn -main
   [& args]
-  (set! *warn-on-reflection* true)
-  (set! *unchecked-math* true)
   (q/sketch
     :title "Mandelbrot fractal"
     :size [WIDTH HEIGHT]
     :setup setup
     :draw #(time (draw-state %))
+    :update update-state
     :middleware [m/fun-mode]
     :feature [:exit-on-close]
     :renderer :p2d))
