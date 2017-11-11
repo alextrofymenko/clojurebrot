19e4d806124b4f7fb0479ca05e5bf07f76d65a95
Alex Trofymenko <byte.shaggy@gmail.com>
2017-11-11 18:42:10 +0000

    Added zoom-out

--- src/clojurebrot/core.clj
+++ src/clojurebrot/core.clj
@@ -10,6 +10,7 @@
 (def HEIGHT 800)
 (def HALF_WIDTH (/ WIDTH 2))
 (def HALF_HEIGHT (/ HEIGHT 2))
+(def LIMIT 4)
 (def ITERATIONS 50)
 (def LOG2 (Math/log 2))
 
@@ -21,7 +22,7 @@
 (defn calculate-hue [n z2] (/ (+ n (- 1 (/ (Math/log (Math/log (Math/sqrt z2))) LOG2))) ITERATIONS))
 
 (defn save-image
-  [{:keys [cx cy scale limit] :as state}]
+  [{:keys [cx cy scale]}]
   (let [bi  (BufferedImage. WIDTH HEIGHT BufferedImage/TYPE_INT_ARGB)
         gfx (.createGraphics bi)]
     (.setBackground gfx (Color. 0 0 0))
@@ -37,25 +38,26 @@
               z2 (z2-fn a' b')]
           (cond
             (> n (dec ITERATIONS)) nil
-            (> z2 limit) (let [hue (calculate-hue n z2)]
+            (> z2 LIMIT) (let [hue (calculate-hue n z2)]
                            (.setColor gfx (Color. (Color/HSBtoRGB hue 0.8 1)))
                            (.fillRect gfx x y 1 1))
             :else (recur a' b' (inc n))))))
-    (ImageIO/write bi "png" (File. "bg.png"))
-    state))
+    (ImageIO/write bi "png" (File. "bg.png"))))
 
 (defn setup []
   (q/frame-rate 30)
   (q/color-mode :rgb)
-  (save-image {:cx    -0.5
-               :cy    0
-               :scale 0.0035
-               :limit 4
-               :mouse {:x1 nil :y1 nil
-                       :x2 nil :y2 nil}}))
+  (let [state {:frames '({:cx    -0.5
+                          :cy    0
+                          :scale 0.0035
+                          :limit 4})
+               :mouse  {:x1 nil :y1 nil
+                        :x2 nil :y2 nil}}]
+    (save-image (first (:frames state)))
+    state))
 
 (defn draw-state
-  [{:keys [cx cy scale limit mouse]}]
+  [{:keys [mouse]}]
   (q/background 0)
   (q/background-image (q/load-image "bg.png"))
   (when (not-any? nil? (vals mouse))
@@ -69,12 +71,14 @@
   (if (= button :left)
     (-> state
         (assoc-in [:mouse :x1] x)
-        (assoc-in [:mouse :y1] y))
+        (assoc-in [:mouse :y1] y)
+        (assoc-in [:mouse :last-button] :left))
     state))
 
 (defn mouse-dragged
-  [{:keys [mouse] :as state} {:keys [x] :as event}]
-  (if (and (:x1 mouse)
+  [{:keys [mouse] :as state} {:keys [x button] :as event}]
+  (if (and (= button :left)
+           (:x1 mouse)
            (> x (:x1 mouse)))
     (-> state
         (assoc-in [:mouse :x2] x)
@@ -85,19 +89,31 @@
 
 (defn mouse-released
   [{{x1 :x1 y1 :y1
-     x2 :x2 y2 :y2} :mouse
-    scale           :scale
-    cx              :cx
-    cy              :cy
-    :as             state} event]
-  (let [new-scale (* scale (/ (- x2 x1) WIDTH))]
-    (-> state
-        (assoc :cx (+ cx (* scale (- (/ (+ x2 x1) 2) HALF_WIDTH)))
-               :cy (+ cy (* scale (- (/ (+ y2 y1) 2) HALF_HEIGHT)))
-               :scale new-scale
-               :mouse {:x1 nil :y1 nil
-                       :x2 nil :y2 nil})
-        (save-image))))
+     x2 :x2 y2 :y2
+     lb :last-button} :mouse
+    frames          :frames
+    :as             state}
+   {:keys [button] :as event}]
+  (if (= lb :left)
+    (let [{:keys [cx cy scale]} (first frames)
+         frame {:cx    (+ cx (* scale (- (/ (+ x2 x1) 2) HALF_WIDTH)))
+                :cy    (+ cy (* scale (- (/ (+ y2 y1) 2) HALF_HEIGHT)))
+                :scale (* scale (/ (- x2 x1) WIDTH))}]
+     (save-image frame)
+     (-> state
+         (update :frames conj frame)
+         (assoc :mouse {:x1 nil :y1 nil
+                        :x2 nil :y2 nil})))
+    state))
+
+(defn mouse-clicked
+  [{:keys [frames] :as state} {:keys [button] :as event}]
+  (if (and (= button :right)
+           (> (count frames) 1))
+    (do
+      (save-image (nth frames 1))
+      (update state :frames #(drop 1 %)))
+    state))
 
 (defn -main
   [& args]
@@ -111,4 +127,5 @@
     :renderer :p2d
     :mouse-pressed mouse-pressed
     :mouse-dragged mouse-dragged
-    :mouse-released mouse-released))
+    :mouse-released mouse-released
+    :mouse-clicked mouse-clicked))
